4c53242e25c9bdbe9363268072aebc014413cfc7
trsvl <vlokh.t4r4s@gmail.com>
2025-02-12 17:36:48 +0200

    update ball throwing logic

---
+++ Assets/Scripts/Gameplay/BallThrowing/Ball.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Gameplay.BallThrowing
+{
+    public class Ball : MonoBehaviour
+    {
+        public Renderer _renderer { get; private set; }
+
+        private SphereDestroyer _sphereDestroyer;
+        private LayerMask _layerMask;
+        private Collider _collider;
+
+
+        public void Init(SphereDestroyer sphereDestroyer, LayerMask layerMask)
+        {
+            _sphereDestroyer = sphereDestroyer;
+            _layerMask = layerMask;
+            _renderer = GetComponent<Renderer>();
+            _collider = GetComponent<Collider>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ball"))
+            {
+                var touchedBallMaterial = collision.gameObject.GetComponent<Renderer>().material;
+                if (_renderer.material.name == touchedBallMaterial.name)
+                {
+                    _sphereDestroyer.TryInitiateWave(collision);
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    _collider.excludeLayers += _layerMask;
+                }
+            }
+        }
+
+        public void InvokeDestroy()
+        {
+            Invoke(nameof(SelfDestruct), 5f);
+        }
+
+        private void SelfDestruct()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Gameplay/BallThrowing/BallThrower.cs
+++ Assets/Scripts/Gameplay/BallThrowing/BallThrower.cs
@@ -1,30 +1,37 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace BallThrowing
+namespace Gameplay.BallThrowing
 {
     public class BallThrower : MonoBehaviour
     {
+        public Material[] materials; //!!!
+
         [SerializeField] private SphereDestroyer sphereDestroyer;
 
-        public GameObject ballPrefab;
+        [Header("Settings")] public GameObject ballPrefab;
         public Transform launchPoint;
         public float forceMultiplier = 0.3f;
         public float maxForce = 50f;
         public float verticalMultiplier = 1.5f;
         public float maxDragDistance = 300f;
-        public LineRenderer lineRenderer;
         public int trajectoryPoints = 40;
         public float timeStep = 0.07f;
         public float respawnDelay = 0.3f;
-        public float verticalForce = 0.5f;
+        public LayerMask collisionMaskLine;
+        public LayerMask collisionMaskBall;
 
+        private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
+        private LineRenderer lineRenderer;
         private Ball currentBall;
         private bool isDragging;
         private Vector3 initialMousePosition;
 
-
-        private void Start() => SpawnBall();
+        private void Start()
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+            SpawnBall();
+        }
 
         private void Update()
         {
@@ -40,6 +47,9 @@ namespace BallThrowing
             {
                 isDragging = true;
                 initialMousePosition = Input.mousePosition;
+                lineRenderer.positionCount = trajectoryPoints;
+                lineRenderer.startColor = currentBall._renderer.material.GetColor(BaseColor);
+                lineRenderer.endColor = currentBall._renderer.material.GetColor(BaseColor);
             }
 
             if (Input.GetMouseButtonUp(0) && isDragging)
@@ -59,18 +69,54 @@ namespace BallThrowing
             Vector3 launchScreenPos = Camera.main.WorldToScreenPoint(launchPoint.position);
             Vector3 screenDelta = currentMousePos - launchScreenPos;
 
-            Vector3 throwDir = new Vector3(-screenDelta.x, screenDelta.magnitude * verticalMultiplier, -screenDelta.y)
-                .normalized;
+            Vector3 throwDir = new Vector3(
+                -screenDelta.x,
+                screenDelta.magnitude * verticalMultiplier,
+                -screenDelta.y
+            ).normalized;
 
             UpdateTrajectoryLine(launchPoint.position, throwDir * forceMagnitude);
+        }
+
+        private void UpdateTrajectoryLine(Vector3 startPos, Vector3 initialVelocity)
+        {
+            if (!lineRenderer) return;
+
+            Vector3[] points = new Vector3[trajectoryPoints];
+            points[0] = startPos;
+            int actualPoints = trajectoryPoints;
+
+            Vector3 currentPosition = startPos;
+            Vector3 currentVelocity = initialVelocity;
+
+            for (int i = 1; i < trajectoryPoints; i++)
+            {
+                currentVelocity += Physics.gravity * timeStep;
+                Vector3 newPosition = currentPosition + currentVelocity * timeStep;
+
+                if (Physics.Linecast(currentPosition, newPosition, out RaycastHit hit, collisionMaskLine))
+                {
+                    points[i] = hit.point;
+                    actualPoints = i + 1;
+                    break;
+                }
+
+                points[i] = newPosition;
+                currentPosition = newPosition;
+            }
+
+            lineRenderer.positionCount = actualPoints;
+            lineRenderer.SetPositions(points);
         }
 
         private void ReleaseBall()
         {
             isDragging = false;
+            lineRenderer.positionCount = 0;
+
+            if (!currentBall) return;
 
             currentBall.InvokeDestroy();
-
             Vector3 currentMousePos = Input.mousePosition;
             Vector3 dragVector = currentMousePos - initialMousePosition;
             float dragMagnitude = Mathf.Clamp(dragVector.magnitude, 0, maxDragDistance);
@@ -79,17 +125,17 @@ namespace BallThrowing
             Vector3 launchScreenPos = Camera.main.WorldToScreenPoint(launchPoint.position);
             Vector3 screenDelta = currentMousePos - launchScreenPos;
 
-            Vector3 throwDir = new Vector3(-screenDelta.x, screenDelta.magnitude * verticalMultiplier, -screenDelta.y)
-                .normalized;
+            Vector3 throwDir = new Vector3(
+                -screenDelta.x,
+                screenDelta.magnitude * verticalMultiplier,
+                -screenDelta.y
+            ).normalized;
 
             ApplyThrowForce(throwDir * forceMagnitude);
-            lineRenderer.positionCount = 0;
         }
 
         private void ApplyThrowForce(Vector3 force)
         {
-            if (!currentBall) return;
-
             Rigidbody rb = currentBall.GetComponent<Rigidbody>();
             if (rb)
             {
@@ -103,32 +149,10 @@ namespace BallThrowing
         private void SpawnBall()
         {
             currentBall = Instantiate(ballPrefab, launchPoint.position, Quaternion.identity).AddComponent<Ball>();
-            currentBall.transform.localScale = Vector3.one * 0.7f;
-            currentBall.tag = "Untagged";
-            currentBall.Init(sphereDestroyer);
+            int randomIndex = Random.Range(0, materials.Length);
+            currentBall.GetComponent<Renderer>().material = materials[randomIndex];
 
-            if (currentBall.TryGetComponent<Rigidbody>(out var rb))
-            {
-                rb.isKinematic = true;
-                rb.drag = 0f;
-            }
-
-            lineRenderer.positionCount = 0;
-        }
-
-        private void UpdateTrajectoryLine(Vector3 startPos, Vector3 initialVelocity)
-        {
-            if (!lineRenderer) return;
-
-            Vector3[] points = new Vector3[trajectoryPoints];
-            for (int i = 0; i < trajectoryPoints; i++)
-            {
-                float t = i * timeStep;
-                points[i] = startPos + initialVelocity * t + Physics.gravity * (0.5f * t * t);
-            }
-
-            lineRenderer.positionCount = trajectoryPoints;
-            lineRenderer.SetPositions(points);
+            currentBall.Init(sphereDestroyer, collisionMaskBall);
         }
 
         private IEnumerator SpawnBallAfterDelay(float delay)
